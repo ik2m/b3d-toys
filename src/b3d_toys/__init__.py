cb71d184f08a1ea7d1c30657fc6c2cc80c8116df
ik2m <39111330+ik2m@users.noreply.github.com>
2025-09-28 14:59:17 +0900

    一部コードを移行

--- src/b3d_toys/__init__.py
+++ src/b3d_toys/__init__.py
@@ -1,10 +1,33 @@
-def main() -> None:
-    print("Hello from b3d-toys!")
+import bpy
+from . import auto_collection_color
+
+class DevPanel(bpy.types.Panel):
+    bl_category = "ik2m"
+    bl_idname = "IK2M_PT_B3dToysPanel"
+    bl_label = "for dev"
+    bl_description = "開発に使うやつ"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    # bl_context = ""
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.operator("script.reload", text="スクリプトリロード", icon="FILE_REFRESH")
 
 
-def register() -> None:
-    return
+
+classes = [DevPanel]
 
 
-def unregister() -> None:
-    return
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    auto_collection_color.register()
+
+
+def unregister():
+    auto_collection_color.unregister()
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
